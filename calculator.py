f595c5f5aadab0f4cefd2cb537ffa0e5d7aa1b99
jobaidahmed <abulkahn20222324@gmail.com>
2024-07-01 12:23:43 -0700

    first commit

---
+++ calculator.py
@@ -0,0 +1,28 @@
+num1  = int(input("Enter the 1st number :"))
+oparetor = input("Enter the oparetor :")
+num2 = int(input("Enter the 2nd number :"))
+
+if oparetor == "+":
+    print(num1+num2)
+elif oparetor == "-":
+    print(num1-num2)
+elif oparetor == "*":
+    print(num1*num2)
+elif oparetor == "/":
+    print(num1/num2)
+elif oparetor == "%":
+    print(num1%num2)
+elif oparetor == "**":
+    print(num1**num2)
+elif oparetor == "//":
+    print(num1//num2)        
+else:
+    print("Invert oparetor")
+    
+
+
+
+
+
+
+
